8dcd781113606f1405b74cb45617ff4d6f66c3ce
Lenin Sanchez <20243ds164@utez.edu.mx>
2026-01-12 15:22:49 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void Main(String[] args) {
+        System.out.println("Nombre: Lenin");
+        System.out.println("Grupo: 2°D");
+        System.out.println("Fecha: 12/01/2026");
+        System.out.println("Hola mundo");
+    }
+}
+
