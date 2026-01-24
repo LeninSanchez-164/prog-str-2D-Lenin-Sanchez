71dca6719601f1c26ad48bf749cd1e6589315dde
Lenin Sanchez <20243ds164@utez.edu.mx>
2026-01-24 10:04:18 -0600

    Entrega de prácticas centralizadas en repositorio oficial

--- proyecto-01-hola-mundo/Main.java
+++ proyecto-01-hola-mundo/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Nombre: Lenin");
         System.out.println("Grupo: 2°D");
         System.out.println("Fecha: 12/01/2026");
