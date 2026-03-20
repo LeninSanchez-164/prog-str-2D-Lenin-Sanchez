a7f44d69d7ea18a6b1a3dda00e4e75179e3fbc40
LeninSanchez-164 <20243ds164@utez.edu.mx>
2026-03-20 09:43:23 -0600

    agregar,bboton orrar y busqueda Âpor correos

--- prog/demo/src/main/java/com/example/demo/controllers/appController.java
+++ prog/demo/src/main/java/com/example/demo/controllers/appController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controllers;
 
-import com.example.demo.services.PersonService;
+import com.example.demo.servicies.PersonService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,57 +17,109 @@ public class appController {
     private ListView<String> listView;
 
     @FXML
+    private TextField txtEdad;
+    @FXML
     private Label lblMsg;
     @FXML
     private TextField txtName;
     @FXML
     private TextField txtEmail;
     @FXML
-    private TextField txtEdad; //
+    private TextField txtBusqueda;
 
     private final ObservableList<String> data = FXCollections.observableArrayList();
     private PersonService service = new PersonService();
 
+
     @FXML
-    public void initialize(){
-        listView.setItems(data);
+    public void initialize(){ // se va a ejecutar el inicia, cuando cargue
+        //Iniciar listview
         loadFromFile();
+        txtBusqueda.textProperty().addListener((observable, oldValue, newValue) -> {
+            filterList(newValue);
+        });
+        listView.getSelectionModel().selectedItemProperty().addListener((obs,oldValue,newValue)->{
+                    loadDataToFrom(newValue);//String con el valor del row 0 test | email@gmail.com | 18
+                }
+        );
+        listView.setItems(data);
     }
 
+    private void filterList(String search) {
+        if (search == null || search.isEmpty()) {
+            initialize();
+            return;
+        }
+        ObservableList<String> filteredList = FXCollections.observableArrayList();
+        String lowerCaseFilter = search.toLowerCase();
+        for (String item : data) {
+            String[] parts = item.split("\\|");
+            String email = parts[1].trim().toLowerCase();
+            if (email.contains(lowerCaseFilter)) {
+                filteredList.add(item);
+            }
+        }
+        listView.setItems(filteredList);
+    }
     @FXML
-    public void onAddPerson(){
-        try{
-            String name = txtName.getText();
-            String email = txtEmail.getText();
-
-            int age = Integer.parseInt(txtEdad.getText());
-
-            service.addPersone(name, email, age);
-
-            lblMsg.setText("Persona agregada con éxito");
+    public void onUpdate() {
+        int index = listView.getSelectionModel().getSelectedIndex();
+        String name = txtName.getText();
+        String email = txtEmail.getText();
+        String edad = txtEdad.getText();
+        try {
+            service.validatePersone(name, email,edad);
+            service.updatePersone(index,name,email,edad);
+            lblMsg.setText("Persona agregada con exito");
             lblMsg.setStyle("-fx-text-fill: green");
-
             txtEmail.clear();
             txtName.clear();
             txtEdad.clear();
             loadFromFile();
-
-        } catch (NumberFormatException e) {
-            // Esto atrapa si el usuario escribe letras en el campo de edad
-            lblMsg.setText("Error: La edad debe ser un número");
-            lblMsg.setStyle("-fx-text-fill: red");
-
         } catch (IOException e) {
-            lblMsg.setText("Hubo un error con el archivo");
+            lblMsg.setText("huno un error con el archivo");
             lblMsg.setStyle("-fx-text-fill: red");
-
-        } catch (IllegalArgumentException ex){
-            // Esto atrapa el error de "Menor de 18 años" (y cualquier otra validación)
-            lblMsg.setText(ex.getMessage());
+        }catch (IllegalArgumentException ex){
+            lblMsg.setText("huno un error con los datos");
             lblMsg.setStyle("-fx-text-fill: red");
         }
     }
 
+    @FXML
+    public void onDelate(){
+        int index = listView.getSelectionModel().getSelectedIndex();
+        try{
+            service.delatePersone(index);
+            loadFromFile();
+            lblMsg.setText("Persona eliminada");
+            lblMsg.setStyle("-fx-text-fill: green");
+        } catch (IOException e) {
+            lblMsg.setText("Hubo un error con el archivo en eliminar");
+            lblMsg.setStyle("-fx-text-fill: red");
+        }
+    }
+    @FXML
+    public void onAddPerson(){
+        try{
+            String name= txtName.getText();
+            String email= txtEmail.getText();
+            String edad = txtEdad.getText();
+
+            service.addPersone(name,email,edad);
+            lblMsg.setText("Persona agregada con exito");
+            lblMsg.setStyle("-fx-text-fill: green");
+            txtEmail.clear();
+            txtName.clear();
+            txtEdad.clear();
+            loadFromFile();
+        } catch (IOException e) {
+            lblMsg.setText("huno un error con el archivo");
+            lblMsg.setStyle("-fx-text-fill: red");
+        }catch (IllegalArgumentException ex){
+            lblMsg.setText("huno un error con los datos");
+            lblMsg.setStyle("-fx-text-fill: red");
+        }
+    }
     @FXML
     private void loadFromFile(){
         try{
@@ -78,5 +131,14 @@ public class appController {
             lblMsg.setText(e.getMessage());
             lblMsg.setStyle("-fx-text-fill: red");
         }
+    }
+    private void loadDataToFrom(String item){
+        if (item == null || item.isEmpty()) {
+            return;
+        }
+        String[] parts =  item.split("\\|");
+        txtName.setText(parts[0]);//Correspondinete al nombre
+        txtEmail.setText(parts[1]);//Correspondinete al correo
+        txtEdad.setText(parts[2]);//Correspondinete a la edad
     }
 }
--- prog/demo/src/main/java/module-info.java
+++ prog/demo/src/main/java/module-info.java
@@ -1,13 +1,15 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
+
+    opens com.example.demo to javafx.fxml;
     opens com.example.demo.controllers to javafx.fxml;
-    opens com.example.demo.services to javafx.fxml;
     opens com.example.demo.repositories to javafx.fxml;
-
+    opens com.example.demo.servicies to javafx.fxml;
     exports com.example.demo;
-    exports com.example.demo.controllers;
-    exports com.example.demo.services;
     exports com.example.demo.repositories;
+    exports com.example.demo.controllers;
+    exports com.example.demo.servicies;
 }
